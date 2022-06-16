9ce94ca7fdcb5515fbc13d44b246f3c62ab09c80
Ben Prentice <b_p149@txstate.edu>
2022-06-16 12:56:09 -0500

    Connect db

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import os
+import sqlite3
+
 from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "db.sqlite")
+
+conn = sqlite3.connect(db_path, check_same_thread=False)
+
+cursor_setup = conn.cursor()
+cursor_setup.execute('CREATE TABLE IF NOT EXISTS users(firstname text, lastname text, email text, username text, password text)')
+conn.commit()
 
 @app.route('/')
 def home():
