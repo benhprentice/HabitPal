42e3c0698b5c8dbe656451f67f0411b1d9c55746
Ben Prentice <b_p149@txstate.edu>
2022-06-17 09:14:43 -0500

    Create welcome page

--- app.py
+++ app.py
@@ -17,12 +17,16 @@ conn.commit()
 cursor_setup.close()
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def welcome():
+    return render_template("welcome.html")
 
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
