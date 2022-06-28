f35972bd118bd4ff8b0e0b0d75c4711b00995768
Ben Prentice <b_p149@txstate.edu>
2022-06-27 22:17:13 -0500

    Clean up code

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ import sqlite3
 from datetime import timedelta, datetime
 from flask import Flask, redirect, render_template, request, session, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
 
 app = Flask(__name__)
 
